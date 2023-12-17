e9445494bf5dc73acbe9107f63548375b8b58ee2
123tordo <153779836+123tordo@users.noreply.github.com>
2023-12-17 17:33:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YouTube Video Player</title>
-</head>
-<body>
-    <div id="player"></div>
-    <script src="https://www.youtube.com/iframe_api"></script>
-    <script src="script.js"></script>
-</body>
-</html>
+et player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: <iframe width="560" height="315" src="https://www.youtube.com/embed/izGwDsrQ1eQ?si=4XtA2VJJ6H2zsLwn&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        // Close the page when the video ends
+        window.close();
+    }
+}
