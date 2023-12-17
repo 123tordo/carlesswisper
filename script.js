cfadf8fc7289029858e9dbcf75b2dd5271f54904
123tordo <153779836+123tordo@users.noreply.github.com>
2023-12-17 18:47:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let player;
 
 function onYouTubeIframeAPIReady() {
+    // Array of YouTube video IDs
+    const videoIds = ['izGwDsrQ1eQ', '8Uee_mcxvrw', '7QcQ6K-Q4H4'];
+
+    // Get a random video ID from the array
+    const randomVideoId = videoIds[Math.floor(Math.random() * videoIds.length)];
+
+    // Create a new player with the random video ID
     player = new YT.Player('player', {
         height: '360',
         width: '640',
-        videoId: 'izGwDsrQ1eQ', // YouTube video ID for "Careless Whisper"
+        videoId: randomVideoId,
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
